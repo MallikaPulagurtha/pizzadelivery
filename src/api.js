2cfabd1b1bad6b9a62f2ba419954be38b291c20a
Mallika <sai.mallika14@gmail.com>
2021-04-16 22:18:01 +0530

    add api

--- src/api.js
+++ src/api.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 
 export function getUser(id) {
-    return axios.get(`http://localhost:2000/${id}`);
+    return axios.get(`https://guvihackathon2.herokuapp.com/${id}`);
 };
 
 export function postPizza(id, pizza) {
-    return axios.post(`http://localhost:2000/${id}`, pizza);
+    return axios.post(`https://guvihackathon2.herokuapp.com/${id}`, pizza);
 };
 
 export function GetPizza(id, pizza) {
-    return axios.post(`http://localhost:2000/${id}`, pizza);
+    return axios.post(`https://guvihackathon2.herokuapp.com/${id}`, pizza);
 };
 
 
 export function postUserRegister(user) {
-    return axios.post("http://localhost:2000/register", user);
+    return axios.post("https://guvihackathon2.herokuapp.com/register", user);
 };
 
 export function postUserLogin(user) {
-    return axios.post("http://localhost:2000/login", user);
+    return axios.post("https://guvihackathon2.herokuapp.com/login", user);
 };
 
